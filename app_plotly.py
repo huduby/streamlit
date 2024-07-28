d1c1aad05fb6e1abebe4fc14b7c852b229ab740e
huduby <79960850+huduby@users.noreply.github.com>
2024-07-28 23:20:35 +0900

    Update app_plotly.py

--- app_plotly.py
+++ app_plotly.py
@@ -1,49 +1,57 @@
+# core pkgs
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-# working with widgets
-# buttons/radio/checkbox/select/
+# import Dat a Viz pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+# import seaborn as sns
 
-# working with buttons
-name = "Jesse"
 
-# 같은 이름으로 위젯을 만들수는 없음.(key를 줘야함)
-if st.button("Submit"):
-    st.write(f"Name : {name.upper()}")
+# matplotlib.use("Agg") # TkAgg
 
-if st.button("Submit",key="new02"):
-    st.write(f"First Name : {name.lower()}")
-st.divider()
-# radiobuttons
-status = st.radio("What is your status",("Active","Inactive"))
-if status == "Active":
-    st.success("You ar active")
-elif status == "Inactive":
-    st.warning("Inactive")
+def main():
+    st.title("Plotting with st.pyplot")
+    df = pd.read_csv("iris.csv")
+    df2 = pd.read_csv("lang_data.csv")
+    st.dataframe(df2.head())
+
+    # previous method
+    # df["spices"].value_counts().plot(kind="bar")
+    # st.pyplot()
     
-st.divider()
-# checkbox
-if st.checkbox("Show/Hide"):
-    st.text("Showing something")
+    # recommend
+    fig,ax = plt.subplots()
+    ax.scatter(*np.random.random(size=(2,100)))
+    st.pyplot(fig)
+    
+    # recommend2
+    fig = plt.figure()
+    df["species"].value_counts().plot(kind="bar")
+    st.pyplot(fig)
+    
+    # recommend3
+    fig,ax=plt.subplots()
+    df["species"].value_counts().plot(kind="bar")
+    st.pyplot(fig)
 
-# expander
-with st.expander("Python"):
-    st.success("Hello Python")
-st.divider()
-
-# select/multi select
-my_lang = ["Python","C","Java","C++","Julia","Go"]
-choice = st.selectbox("Language",my_lang)
-st.write("You selected ", choice)
-
-# multiple selection
-spoken_lang = ("English","Korea","French","Spanish")
-my_spoken_lang = st.multiselect("Spoken Lang",spoken_lang,default="English")
-
-# slider 
-# numbers ( int/float )
-age = st.slider("Age",1,100,20)
-st.write(age)
-
-# Any data
-# select slider
-color = st.select_slider("Choose Color",options=["red","yellow","blue","purple"],value=("red","yellow"))
+    fig =plt.figure()
+    sns.countplot(df["species"])
+    st.pyplot(fig)    
+    
+    st.bar_chart(df[["sepal_length","petal_length"]])
+    
+    st.divider()
+    
+    lang_list = df2.columns[1:].tolist()
+    # st.write(lang_list)
+    lang_choices = st.multiselect("Language",lang_list,default="Python")
+    new_df = df2[lang_choices]
+    st.line_chart(new_df)
+    
+    st.area_chart(new_df)
+if __name__ == "__main__":
+    main()
+    
+    
